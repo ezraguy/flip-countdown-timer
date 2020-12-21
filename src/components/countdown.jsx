aa4ea1dcb26a0c55a24e67944597c784794068db
ezraguy <55058653+ezraguy@users.noreply.github.com>
2020-12-21 13:55:05 +0200

    animation fix

--- src/components/countdown.jsx
+++ src/components/countdown.jsx
@@ -1,52 +1,112 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 const CountDown = () => {
+    const [seconds, setSeconds] = useState(59)
+    const [minutes, setMinutes] = useState(59)
+    const [hours, setHours] = useState(23)
+    const [days, setDays] = useState(8)
+
+    const [countdowns, setCountdowns] = useState([
+        { id: 1, classNames: 'time-left', wrapperClassName: 'time-top', desc: 'Days' },
+        { id: 2, classNames: 'time-left', wrapperClassName: 'time-top', desc: 'Hours' },
+        { id: 3, classNames: 'time-left', wrapperClassName: 'time-top', desc: 'Minutes' },
+        { id: 4, classNames: 'time-left fold always ', wrapperClassName: 'time-top flip always ', desc: 'Seconds' },
+    ]);
+
+    const startCountDown = () => {
+
+
+        let sec = 59;
+        let min = 59;
+        let hours = 23;
+        let days = 8;
+        let temp = [...countdowns]
+
+        setInterval(() => {
+            sec = sec - 1;
+            if (sec === 0) {
+                temp[2].classNames = 'time-left fold'
+                temp[2].wrapperClassName = 'time-top flip'
+                setTimeout(() => {
+                    min = min - 1;
+                }, 900);
+                sec = 59
+
+            }
+            if (min === 0) {
+                temp[1].classNames = 'time-left fold'
+                temp[1].wrapperClassName = 'time-top flip'
+                setTimeout(() => {
+
+                    hours = hours - 1;
+                }, 900);
+                min = 59
+
+            }
+            if (hours === 0) {
+                temp[0].classNames = 'time-left fold'
+                temp[0].wrapperClassName = 'time-top flip'
+                setTimeout(() => {
+
+                    days = days - 1;
+                }, 900);
+                hours = 23
+            }
+
+            setSeconds(sec)
+            setCountdowns(temp);
+            setMinutes(min)
+            setHours(hours)
+            setDays(days)
+            resetClassNames();
+
+
+
+        }, 1000);
+    }
+    const resetClassNames = () => {
+        let temp = [...countdowns];
+        for (let i = 0; i < temp.length; i++) {
+            if (i === 3)
+                continue
+            const element = temp[i];
+            element.classNames = 'time-left';
+            element.wrapperClassName = 'time-top'
+
+        }
+    }
+    useEffect(() => {
+        startCountDown();
+
+    }, [])
     return (
         <div className="countdown">
             <div className="timer-wrap">
-                <div className="days ">
 
-                    <div className="time">
-                        <div className="half-circle-left"></div>
-                        <div className="time-top"></div>
-                        <div className="fake-time-top"></div>
-                        <div className="time-bottom"></div>
-                        <div className="half-circle-right"></div>
-                    </div>
+                {countdowns.map(({ id, classNames, starterTime, wrapperClassName, desc, }, index) => {
+                    return (
+                        <div className={desc.toLowerCase()} key={id}>
 
-                    <p className="desc">Days</p>
-                </div>
-                <div className="hours ">
-                    <div className="time">
-                        <div className="half-circle-left"></div>
-                        <div className="time-top"></div>
-                        <div className="fake-time-top"></div>
-                        <div className="time-bottom"></div>
-                        <div className="half-circle-right"></div>
-                    </div>
-                    <p className="desc">hours</p>
-                </div>
-                <div className="minutes ">
-                    <div className="time">
-                        <div className="half-circle-left"></div>
-                        <div className="time-top"></div>
-                        <div className="fake-time-top"></div>
-                        <div className="time-bottom"></div>
-                        <div className="half-circle-right"></div>
-                    </div>
-                    <p className="desc">minutes</p>
-                </div>
-                <div className="seconds ">
-                    <div className="time">
-                        <div className="half-circle-left"></div>
-                        <div className="fake-time-top"></div>
-                        <div className="time-top"></div>
-                        <div className="time-bottom"></div>
-                        <div className="half-circle-right"></div>
-                    </div>
-                    <p className="desc">seconds</p>
-                </div>
+                            <div className="time" >
+                                <span className={classNames} >
+                                    {index === 3 ? seconds : index === 2 ? minutes : index === 1 ? hours : days}
+                                </span>
+                                <div className="half-circle-left"></div>
+                                <div className={wrapperClassName}></div>
+                                <div className="fake-time-top"></div>
+                                <div className="time-bottom"></div>
+                                <div className="half-circle-right"></div>
+                            </div>
+
+                            <p className="desc">{desc}</p>
+                        </div>
+                    )
+                })}
+
+
+
+
             </div>
-        </div>);
+        </div >);
 }
 
 export default CountDown;
